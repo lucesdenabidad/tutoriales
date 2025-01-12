d2b5fdd7ed4df040c73b7695612b9ea35d0f7e62
lucesdenabidad <ct.bopi@gmail.com>
2025-01-11 22:36:08 -0300

    all

---
+++ PluginTutorial/src/main/java/datta/dev/pluginTutorial/Main.java
@@ -0,0 +1,18 @@
+package datta.dev.pluginTutorial;
+
+import datta.dev.pluginTutorial.listeners.BlockBreakListener;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(new BlockBreakListener(), this);
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
